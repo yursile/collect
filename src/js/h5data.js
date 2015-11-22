7ece22e46b12af5f53c0d24084dec609aa5992f1
yursile <289181450@qq.com>
2015-11-22 23:23:47 +0800

    share

---
+++ src/js/h5data.js
@@ -0,0 +1,60 @@
+var h5_data = [
+	        {
+	        	img:'img/one.png',
+	        	content:'一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	number:'No.397'
+	        },{
+	        	img:'img/one.png',
+	        	content:'一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	number:'No.399'
+	        },
+	        {
+	        	img:'img/one.png',
+	        	content:'一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	number:'No.397'
+	        },
+	        {
+	        	img:'img/one.png',
+	        	content:'一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	number:'No.399'
+	        },
+	        {
+	        	img:'img/one.png',
+	        	content:'一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	number:'No.397'
+	        },
+	        {
+	        	img:'img/one.png',
+	        	content:'一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	number:'No.399'
+	        },
+	        {
+	        	img:'img/one.png',
+	        	content:'一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	number:'No.397'
+	        },
+	        {
+	        	img:'img/one.png',
+	        	content:'一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	number:'No.399'
+	        },
+	        {
+	        	img:'img/one.png',
+	        	content:'一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	number:'No.397'
+	        },
+	        {
+	        	img:'img/one.png',
+	        	content:'一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	number:'No.399'
+	        },
+	        {
+	        	img:'img/one.png',
+	        	content:'一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	number:'No.397'
+	        },
+	        {
+	        	img:'img/one.png',
+	        	content:'一秒钟到底有多长?HOW LONG IS ONE SECOND?一秒钟到底有多长?HOW LONG IS ONE SECOND?',
+	        	number:'No.399'
+	        }];
